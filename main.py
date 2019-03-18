9b97fb7b69aa116714b5aadc7f78f12a3052887d
Satoshi Deguchi <deguchik@gmail.com>
2019-03-18 14:29:30 +0900

    リファクタ

--- main.py
+++ main.py
@@ -16,79 +16,88 @@ import re
 from collections import namedtuple
 from itertools import takewhile
 
-items = []
-with codecs.open('zips/ndc9.ttl', 'r', 'utf-8') as file:
-    for line in file:
-        if line.startswith('ndc9:'):
-            buf = [line]
-            buf.extend(takewhile(lambda x: x.strip()!='.', file))
-            new_buf = []
-            isNDCV = False
-            ndcv_buf = []
-            for b in buf[1:]:
-                b = b.strip()
-                if b=='ndcv:indexedTerm [':
-                    isNDCV = True
-                    ndcv_buf.append(b)
-                elif b=='] ;':
-                    isNDCV = False
-                    ndcv_buf.append(b)
-                else:
-                    if isNDCV:
-                        ndcv_buf.append(b)
+def getNDCItems(ndc_name):
+    items = []
+    with codecs.open('zips/' + ndc_name + '.ttl', 'r', 'utf-8') as file:
+        for line in file:
+            if line.startswith(ndc_name + ':'):
+                tmp_buff = [line]
+                tmp_buff.extend(takewhile(lambda x: x.strip()!='.', file))
+                # print(''.join(tmp_buff))
+                # print('---------------------------------------------')
+
+                # indexedTermだけbufferを分離
+                buff = []
+                isIndexedTerm = False
+                it_buff = []
+                for b in tmp_buff[1:]:
+                    b = b.strip()
+                    if b=='ndcv:indexedTerm [':
+                        isIndexedTerm = True
+                        it_buff.append(b)
+                    elif b=='] ;':
+                        isIndexedTerm = False
+                        it_buff.append(b)
                     else:
-                        new_buf.append(b)
-            # print(''.join(buf))
-            # print('---------------------------------------------')
-            
-            item = {}
-
-            # 1行目の処理 ex) ndc9:000 a ndcv:Section, skos:Concept ;
-            ndm = re.search(r'(ndc[8|9]):([^ndcv|skos]+)', line)
-            if ndm:
-                item[ndm.groups()[0]] = ndm.groups()[1].strip()
-            nm = re.search(r'ndcv:([^\s]+)', line)
-            if nm:
-                item['ndcv'] = nm.groups()[0].replace(',', '').strip()
-            sm = re.search(r'skos:([^\s]+)', line)
-            if sm:
-                item['skos'] = sm.groups()[0].strip()
-
-            for b in new_buf:
-                m = re.match(r'[^:]+:([^\s]+) ([^;]+) ;', b)
-                if m:
-                    props = m.groups()
-                    key = props[0]
-                    value = props[1]
-                    if key=='label' or key=='note':
-                        lm = re.search(r'"([^"]+)"', value)
-                        if lm:
-                            value = lm.groups()[0]
-                    if key=='prefLabel':
-                        lm = re.search(r'"([^"]+)"@ja', value)
-                        if lm:
-                            item['prefLabel@ja'] = lm.groups()[0]
-                        lm = re.search(r'"([^"]+)"@en', value)
-                        if lm:
-                            item['prefLabel@ja'] = lm.groups()[0]
-                        continue
-                    if key=='relatedMatch' or key=='seeAlso':
-                        value = [x.strip() for x in value.split(',')]
-                    item[key] = value
-            for n in ndcv_buf:
-                m = re.match(r'[^:]+:([^\s]+) ([^;]+) ;', n)
-                if m:
-                    ndvc_items = []
-                    for ndvc_item in m.groups():
-                        lm = re.search(r'"([^"]+)"', ndvc_item)
-                        if lm:
-                            ndvc_items.append(lm.groups()[0])
-                        else:
-                            ndvc_items.append(ndvc_item)
-                    item['indexedTerm'] = ndvc_items
+                        if isIndexedTerm:
+                            it_buff.append(b)
+                            buff.append(b)
                 
-            print(item)
-            items.append(item)
+                item = {}
+
+                # 1行目の処理 ex) ndc9:000 a ndcv:Section, skos:Concept ;
+                ndm = re.search(r'ndc[8|9]:([^ndcv|skos]+)', line)
+                if ndm:
+                    item['ndc'] = ndm.groups()[0].strip()
+                nm = re.search(r'ndcv:([^\s]+)', line)
+                if nm:
+                    item['ndcv'] = nm.groups()[0].replace(',', '').strip()
+                sm = re.search(r'skos:([^\s]+)', line)
+                if sm:
+                    item['skos'] = sm.groups()[0].strip()
+
+                # 各行処理
+                for b in buff:
+                    m = re.match(r'[^:]+:([^\s]+) ([^;]+) ;', b)
+                    if m:
+                        props = m.groups()
+                        key = props[0]
+                        value = props[1]
+                        if key=='label' or key=='note':
+                            lm = re.search(r'"([^"]+)"', value)
+                            if lm:
+                                value = lm.groups()[0].split('．')
+                        if key=='prefLabel':
+                            lm = re.search(r'"([^"]+)"@ja', value)
+                            if lm:
+                                item['prefLabel@ja'] = lm.groups()[0]
+                            lm = re.search(r'"([^"]+)"@en', value)
+                            if lm:
+                                item['prefLabel@ja'] = lm.groups()[0]
+                            continue
+                        if key=='relatedMatch' or key=='seeAlso':
+                            value = [x.strip() for x in value.split(',')]
+                        item[key] = value
+
+                # indexedTermの処理
+                for n in it_buff:
+                    m = re.match(r'[^:]+:([^\s]+) ([^;]+) ;', n)
+                    if m:
+                        ndvc_items = []
+                        for ndvc_item in m.groups():
+                            lm = re.search(r'"([^"]+)"', ndvc_item)
+                            if lm:
+                                ndvc_items.append(lm.groups()[0])
+                            else:
+                                ndvc_items.append(ndvc_item)
+                        item['indexedTerm'] = ndvc_items
+                    
+                # print(item)
+                items.append(item)
+    return items
+
+ndc8_items = getNDCItems('ndc8')
+ndc9_items = getNDCItems('ndc9')
 
 # f = codecs.open('zips/sample.ttl', 'r', 'utf-8')
 # ndc = f.read()
@@ -97,9 +106,17 @@ with codecs.open('zips/ndc9.ttl', 'r', 'utf-8') as file:
 
 @api.route("/")
 def index(req, resp):
+    resp.text = api.template('index.html')
+
+@api.route("/ndc8")
+def index(req, resp):
     resp.headers = {"Content-Type": "application/json; charset=utf-8"}
-    resp.content = json.dumps(items, ensure_ascii=False)
-    # resp.text = "hello, world!"
+    resp.content = json.dumps(ndc8_items[:100], ensure_ascii=False)
+
+@api.route("/ndc9")
+def index(req, resp):
+    resp.headers = {"Content-Type": "application/json; charset=utf-8"}
+    resp.content = json.dumps(ndc9_items[:100], ensure_ascii=False)
 
 if __name__ == '__main__':
     api.run()
