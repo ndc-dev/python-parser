03dc0cd55447d21dc6b1d0cbf02ced7cf35a706c
Satoshi Deguchi <deguchik@gmail.com>
2019-03-18 17:17:43 +0900

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import copy
 import codecs
 import zipfile
 import responder
@@ -16,11 +17,15 @@ import re
 from collections import namedtuple
 from itertools import takewhile
 
-def getNDCItems(ndc_name):
+def get_ndc_items(ndc_name):
     items = []
     with codecs.open('zips/' + ndc_name + '.ttl', 'r', 'utf-8') as file:
+        section_count = 0
         for line in file:
             if line.startswith(ndc_name + ':'):
+                section_count += 1
+                if section_count==1: # トップは不要データなので飛ばす
+                    continue
                 tmp_buff = [line]
                 tmp_buff.extend(takewhile(lambda x: x.strip()!='.', file))
                 # print(''.join(tmp_buff))
@@ -41,6 +46,7 @@ def getNDCItems(ndc_name):
                     else:
                         if isIndexedTerm:
                             it_buff.append(b)
+                        else:
                             buff.append(b)
                 
                 item = {}
@@ -48,7 +54,7 @@ def getNDCItems(ndc_name):
                 # 1行目の処理 ex) ndc9:000 a ndcv:Section, skos:Concept ;
                 ndm = re.search(r'ndc[8|9]:([^ndcv|skos]+)', line)
                 if ndm:
-                    item['ndc'] = ndm.groups()[0].strip()
+                    item['ndc'] = ndm.groups()[0].strip().split(' ')[0] # 000 a の a を削除
                 nm = re.search(r'ndcv:([^\s]+)', line)
                 if nm:
                     item['ndcv'] = nm.groups()[0].replace(',', '').strip()
@@ -63,7 +69,7 @@ def getNDCItems(ndc_name):
                         props = m.groups()
                         key = props[0]
                         value = props[1]
-                        if key=='label' or key=='note':
+                        if key=='label' or key=='note' or key=='notation':
                             lm = re.search(r'"([^"]+)"', value)
                             if lm:
                                 value = lm.groups()[0].split('．')
@@ -94,10 +100,50 @@ def getNDCItems(ndc_name):
                     
                 # print(item)
                 items.append(item)
+                # if section_count > 2:
+                #     break
     return items
 
-ndc8_items = getNDCItems('ndc8')
-ndc9_items = getNDCItems('ndc9')
+
+def get_category_number(ndc):
+    return ndc.split(' ')[0].split('.')[0].split('_')[0] # 071_07 対策
+
+def get_parallel_labels(items):
+    parallel_labels = {}
+    for item in items:
+        category_number = get_category_number(item['ndc'])
+        if category_number not in parallel_labels:
+            parallel_labels[category_number] = [item['ndc']]
+        else:
+            parallel_labels[category_number].append(item['ndc'])
+    return parallel_labels
+
+def get_items(ndc):
+    items = get_ndc_items(ndc)
+    parallel_labls = get_parallel_labels(items)
+    items_dict = {}
+    for item in items:
+        category_number = get_category_number(item['ndc'])
+        if len(category_number)<=2:
+            items_dict[category_number] = {
+                'ndc': item['ndc'],
+                'label': item['label'] if 'label' in item else ''
+            }
+    for item in items:
+        category_number = get_category_number(item['ndc'])
+        item['paralell_labels'] = []
+        item['upper_level_labels'] = []
+        if category_number in parallel_labls:
+            parallel_labels = parallel_labls[category_number]
+            item['paralell_labels'] = [label for label in parallel_labels if label != item['ndc']]
+        item['upper_level_labels'] = []
+        while len(category_number)>1:
+            category_number = category_number[:-1]
+            item['upper_level_labels'].append(items_dict[category_number])
+    return items
+
+ndc8_items = get_items('ndc8')
+ndc9_items = get_items('ndc9')
 
 # f = codecs.open('zips/sample.ttl', 'r', 'utf-8')
 # ndc = f.read()
@@ -113,10 +159,22 @@ def index(req, resp):
     resp.headers = {"Content-Type": "application/json; charset=utf-8"}
     resp.content = json.dumps(ndc8_items[:100], ensure_ascii=False)
 
+@api.route("/ndc8_labels")
+def index(req, resp):
+    resp.headers = {"Content-Type": "application/json; charset=utf-8"}
+    resp.content = json.dumps(ndc8_parallel_labls, ensure_ascii=False)
+
+
 @api.route("/ndc9")
 def index(req, resp):
     resp.headers = {"Content-Type": "application/json; charset=utf-8"}
     resp.content = json.dumps(ndc9_items[:100], ensure_ascii=False)
+
+@api.route("/ndc9_labels")
+def index(req, resp):
+    resp.headers = {"Content-Type": "application/json; charset=utf-8"}
+    resp.content = json.dumps(ndc9_parallel_labls, ensure_ascii=False)
+
 
 if __name__ == '__main__':
     api.run()
