be5b9bd8d241b80920d91027fe4991c424c170d2
deguchi <deguchik@gmail.com>
2019-04-05 13:38:38 +0900

    update  readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# NDC Parser
+
+NDC Parser for developer
+
+## build
+pip install wheel  
+python setup.py bdist_wheel
+
+## Sample
+[sample.py](https://github.com/ndc-dev/python-parser/blob/master/sample.py)
+
+## Demo
+[https://ndc-api-beta.arukascloud.io/](https://ndc-api-beta.arukascloud.io/)
--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+import os
+import io
 from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+def get_readme():
+    path = os.path.join(here, 'README.md')
+    with io.open(path, encoding='utf-8') as f:
+        return '\n' + f.read()
 
 setup(
     name='ndc_parser',
     version='0.1',
     description='NDC Parser',
+    long_description=get_readme(),
+    long_description_content_type='text/markdown',
     url='https://github.com/ndc-dev/python-parser',
     author='CALIL Inc.',
     author_email='info@calil.jp',
